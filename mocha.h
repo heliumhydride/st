00d8d472b40a4784de8700d3c3e541d2d2054800
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-07-31 12:33:00 +0200

    config.h: oops wrong background

--- mocha.h
+++ mocha.h
@@ -34,4 +34,4 @@ unsigned int defaultfg = 256;
 unsigned int defaultbg = 257;
 unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 258;
-unsigned int background = 258;
+unsigned int background = 257;
